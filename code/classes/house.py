194d7eb6b07b2b6d834868d42cad4dbf6c356e1c
Ysann3 <70634407+Ysann3@users.noreply.github.com>
2021-06-03 23:30:49 +0200

    Update house.py
    added load_houses

--- code/classes/house.py
+++ code/classes/house.py
@@ -3,3 +3,20 @@ class House():
         self.x = x
         self.y = y
         self.max_output = max_output
+        
+    def load_houses(house_file):
+        """
+        Load the position of the houses and their max output.
+        """
+        house_coordinatesX = {}
+        house_coordinatesY = {}
+        house_max_output = {}
+        with open(house_file, 'r') as in_file:
+            reader = csv.DictReader(in_file)
+
+            for count, row in enumerate(reader):
+                house_coordinatesX[count+1] = row['x']
+                house_coordinatesY[count+1] = row['x']
+                house_max_output[count+1] = row['maxoutput']
+
+        return house_coordinatesX, house_coordinatesX, house_max_output
